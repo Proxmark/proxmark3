031311c7ae0b3be39da2768ca6871beac3062ec6
etmatrix <etmatrix@etmatrix>
2015-10-25 08:10:17 +0100

    LED D is on while snoop, when user button is pressed snoop is stopped

--- armsrc/hfsnoop.c
+++ armsrc/hfsnoop.c
@@ -9,7 +9,7 @@ static void RAMFUNC optimizedSnoop(void)
 {
 	BigBuf_free();
 	int n = BigBuf_max_traceLen() / sizeof(uint16_t); // take all memory
-        
+
 	uint16_t *dest = (uint16_t *)BigBuf_get_addr();
 	uint16_t *destend = dest + n;
 
@@ -24,14 +24,14 @@ static void RAMFUNC optimizedSnoop(void)
 		}
 	}
 	//Resetting Frame mode (First set in fpgaloader.c)
-	AT91C_BASE_SSC->SSC_RFMR = SSC_FRAME_MODE_BITS_IN_WORD(8) | AT91C_SSC_MSBF | SSC_FRAME_MODE_WORDS_PER_TRANSFER(0); 
+	AT91C_BASE_SSC->SSC_RFMR = SSC_FRAME_MODE_BITS_IN_WORD(8) | AT91C_SSC_MSBF | SSC_FRAME_MODE_WORDS_PER_TRANSFER(0);
 }
 
 void HfSnoop(int samplesToSkip, int triggersToSkip)
 {
 	Dbprintf("Skipping first %d sample pairs, Skipping %d triggers.\n", samplesToSkip, triggersToSkip);
 	bool trigger_cnt;
-	LED_D_OFF();
+	LED_D_ON();
 	// Select correct configs
 	FpgaDownloadAndGo(FPGA_BITSTREAM_HF);
 	// Set up the synchronous serial port
@@ -42,32 +42,34 @@ void HfSnoop(int samplesToSkip, int triggersToSkip)
 	SpinDelay(100);
 
 	AT91C_BASE_SSC->SSC_RFMR = SSC_FRAME_MODE_BITS_IN_WORD(16); // Setting Frame Mode For better performance on high speed data transfer.
-  
+
 	trigger_cnt = 0;
-	uint16_t r;
-	for(;;) {
+	uint16_t r = 0;
+	while(!BUTTON_PRESS()) {
+		WDT_HIT();
 		if(AT91C_BASE_SSC->SSC_SR & (AT91C_SSC_RXRDY)) {
 			r = (uint16_t)AT91C_BASE_SSC->SSC_RHR;
-			if (!(trigger_cnt == triggersToSkip) && ( (r >> 8) >= 240))
+			if (!(trigger_cnt == triggersToSkip) && ( (r >> 8) >= 240)) 
 			{
 				Dbprintf("Trigger kicked! Value: %d.", r >> 8);
 				trigger_cnt++;
 				break;
+			} 
+		}
+	}
+	if(!BUTTON_PRESS()) {
+		Dbprintf("Trigger kicked! Value: %d, Dumping Samples Hispeed now.", r >> 8);
+		int waitcount = samplesToSkip; // lets wait 40000 ticks of pck0
+		while(waitcount != 0) {
+			if(AT91C_BASE_SSC->SSC_SR & (AT91C_SSC_RXRDY)) {
+				waitcount--;
 			}
 		}
-	}
-	Dbprintf("Trigger kicked! Value: %d, Dumping Samples Hispeed now.", r >> 8);
-	int waitcount = samplesToSkip; // lets wait 40000 ticks of pck0
-	while(waitcount != 0) {
-		if(AT91C_BASE_SSC->SSC_SR & (AT91C_SSC_RXRDY)) {
-			waitcount--;
-		}
+
+		optimizedSnoop();
 	}
 
-	// Snooooop!!!
-	optimizedSnoop();
-
-	DbpString("Done.");
+	DbpString("HF Snoop end");
 	FpgaWriteConfWord(FPGA_MAJOR_MODE_OFF);
 	LED_D_OFF();
 }
