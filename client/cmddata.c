de53156e1ea6982f03a6c39adf1ae17a57056132
marshmellow42 <marshmellowrf@gmail.com>
2016-06-19 23:44:54 -0400

    Fix the AskEdgeDetect cleaning tool
    somehow it got broken.
    (maybe i had an incomplete pull request once upon a time)

--- client/cmddata.c
+++ client/cmddata.c
@@ -879,13 +879,15 @@ int CmdGraphShiftZero(const char *Cmd)
 int CmdAskEdgeDetect(const char *Cmd)
 {
 	int thresLen = 25;
+	int Last = 0;
 	sscanf(Cmd, "%i", &thresLen); 
 
 	for(int i = 1; i<GraphTraceLen; i++){
 		if (GraphBuffer[i]-GraphBuffer[i-1]>=thresLen) //large jump up
-			GraphBuffer[i-1] = 127;
+			Last = 127;
 		else if(GraphBuffer[i]-GraphBuffer[i-1]<=-1*thresLen) //large jump down
-			GraphBuffer[i-1] = -127;
+			Last = -127;
+		GraphBuffer[i-1] = Last;
 	}
 	RepaintGraphWindow();
 	return 0;
