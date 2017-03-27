de77d4acde77e4a7fbd36295dfb54a31353eb0d8
pwpiwi <pwpiwi@users.noreply.github.com>
2017-03-27 10:28:06 +0200

    add: iceman1001's idea to reuse @pwpiwi's fast select without anticollision to speedup hf mf chk (see http://www.proxmark.org/forum/viewtopic.php?id=2920)

--- armsrc/iso14443a.h
+++ armsrc/iso14443a.h
@@ -13,63 +13,9 @@
 #ifndef __ISO14443A_H
 #define __ISO14443A_H
 
-#include "common.h"
-#include "mifaresniff.h"
-
-typedef struct {
-	enum {
-		DEMOD_UNSYNCD,
-		// DEMOD_HALF_SYNCD,
-		// DEMOD_MOD_FIRST_HALF,
-		// DEMOD_NOMOD_FIRST_HALF,
-		DEMOD_MANCHESTER_DATA
-	} state;
-	uint16_t twoBits;
-	uint16_t highCnt;
-	uint16_t bitCount;
-	uint16_t collisionPos;
-	uint16_t syncBit;
-	uint8_t  parityBits;
-	uint8_t  parityLen;
-	uint16_t shiftReg;
-	uint16_t samples;
-	uint16_t len;
-	uint32_t startTime, endTime;
-	uint8_t  *output;
-	uint8_t  *parity;
-} tDemod;
-
-typedef enum {
-	MOD_NOMOD = 0,
-	MOD_SECOND_HALF,
-	MOD_FIRST_HALF,
-	MOD_BOTH_HALVES
-	} Modulation_t;
-
-typedef struct {
-	enum {
-		STATE_UNSYNCD,
-		STATE_START_OF_COMMUNICATION,
-		STATE_MILLER_X,
-		STATE_MILLER_Y,
-		STATE_MILLER_Z,
-		// DROP_NONE,
-		// DROP_FIRST_HALF,
-		} state;
-	uint16_t shiftReg;
-	int16_t	 bitCount;
-	uint16_t len;
-	uint16_t byteCntMax;
-	uint16_t posCnt;
-	uint16_t syncBit;
-	uint8_t  parityBits;
-	uint8_t  parityLen;
-	uint32_t fourBits;
-	uint32_t startTime, endTime;
-    uint8_t *output;
-	uint8_t *parity;
-} tUart;
-
+#include <stdint.h>
+#include <stdbool.h>
+#include "mifare.h"
 
 extern void GetParity(const uint8_t *pbtCmd, uint16_t len, uint8_t *par);
 extern void AppendCrc14443a(uint8_t *data, int len);
@@ -81,7 +27,6 @@ extern int ReaderReceive(uint8_t *receivedAnswer, uint8_t *par);
 
 extern void iso14443a_setup(uint8_t fpga_minor_mode);
 extern int iso14_apdu(uint8_t *cmd, uint16_t cmd_len, void *data);
-extern int iso14443a_select_card(uint8_t *uid_ptr, iso14a_card_select_t *resp_data, uint32_t *cuid_ptr);
+extern int iso14443a_select_card(uint8_t *uid_ptr, iso14a_card_select_t *resp_data, uint32_t *cuid_ptr, bool anticollision, uint8_t num_cascades);
 extern void iso14a_set_trigger(bool enable);
-
 #endif /* __ISO14443A_H */
