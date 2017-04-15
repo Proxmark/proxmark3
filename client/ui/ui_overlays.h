2c441f573c32b11e5764b3bfb70b64e289314267
marshmellow42 <marshmellowrf@gmail.com>
2017-04-15 13:12:56 -0400

    attempt ui_overlays.h qt4 version

--- client/ui/ui_overlays.h
+++ client/ui/ui_overlays.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'overlays.ui'
 **
-** Created by: Qt User Interface Compiler version 5.6.1
+** Created by: Qt User Interface Compiler version 4.8.6
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -10,19 +10,19 @@
 #define OVERLAYS_H
 
 #include <QtCore/QVariant>
-#include <QtWidgets/QAction>
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QButtonGroup>
-#include <QtWidgets/QFormLayout>
-#include <QtWidgets/QHBoxLayout>
-#include <QtWidgets/QHeaderView>
-#include <QtWidgets/QLabel>
-#include <QtWidgets/QPushButton>
-#include <QtWidgets/QSlider>
-#include <QtWidgets/QSpacerItem>
-#include <QtWidgets/QTabWidget>
-#include <QtWidgets/QVBoxLayout>
-#include <QtWidgets/QWidget>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QFormLayout>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QHeaderView>
+#include <QtGui/QLabel>
+#include <QtGui/QPushButton>
+#include <QtGui/QSlider>
+#include <QtGui/QSpacerItem>
+#include <QtGui/QTabWidget>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QWidget>
 
 QT_BEGIN_NAMESPACE
 
@@ -65,26 +65,26 @@ public:
     void setupUi(QWidget *Form)
     {
         if (Form->objectName().isEmpty())
-            Form->setObjectName(QStringLiteral("Form"));
+            Form->setObjectName(QString::fromUtf8("Form"));
         Form->resize(614, 286);
         verticalLayout_3 = new QVBoxLayout(Form);
-        verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
+        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
         tabWidget_overlays = new QTabWidget(Form);
-        tabWidget_overlays->setObjectName(QStringLiteral("tabWidget_overlays"));
+        tabWidget_overlays->setObjectName(QString::fromUtf8("tabWidget_overlays"));
         tab = new QWidget();
-        tab->setObjectName(QStringLiteral("tab"));
+        tab->setObjectName(QString::fromUtf8("tab"));
         tab->setFocusPolicy(Qt::StrongFocus);
         verticalLayout_2 = new QVBoxLayout(tab);
-        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
         formLayout = new QFormLayout();
-        formLayout->setObjectName(QStringLiteral("formLayout"));
+        formLayout->setObjectName(QString::fromUtf8("formLayout"));
         label_5 = new QLabel(tab);
-        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setObjectName(QString::fromUtf8("label_5"));
 
         formLayout->setWidget(0, QFormLayout::FieldRole, label_5);
 
         label = new QLabel(tab);
-        label->setObjectName(QStringLiteral("label"));
+        label->setObjectName(QString::fromUtf8("label"));
 
         formLayout->setWidget(0, QFormLayout::LabelRole, label);
 
@@ -92,7 +92,7 @@ public:
         verticalLayout_2->addLayout(formLayout);
 
         horizontalSlider_window = new QSlider(tab);
-        horizontalSlider_window->setObjectName(QStringLiteral("horizontalSlider_window"));
+        horizontalSlider_window->setObjectName(QString::fromUtf8("horizontalSlider_window"));
         horizontalSlider_window->setMinimum(10);
         horizontalSlider_window->setMaximum(10000);
         horizontalSlider_window->setValue(2000);
@@ -106,19 +106,19 @@ public:
 
         tabWidget_overlays->addTab(tab, QString());
         tab_3 = new QWidget();
-        tab_3->setObjectName(QStringLiteral("tab_3"));
+        tab_3->setObjectName(QString::fromUtf8("tab_3"));
         verticalLayout_4 = new QVBoxLayout(tab_3);
-        verticalLayout_4->setObjectName(QStringLiteral("verticalLayout_4"));
+        verticalLayout_4->setObjectName(QString::fromUtf8("verticalLayout_4"));
         formLayout_4 = new QFormLayout();
-        formLayout_4->setObjectName(QStringLiteral("formLayout_4"));
+        formLayout_4->setObjectName(QString::fromUtf8("formLayout_4"));
         formLayout_4->setContentsMargins(-1, -1, -1, 0);
         label_8 = new QLabel(tab_3);
-        label_8->setObjectName(QStringLiteral("label_8"));
+        label_8->setObjectName(QString::fromUtf8("label_8"));
 
         formLayout_4->setWidget(0, QFormLayout::LabelRole, label_8);
 
         label_9 = new QLabel(tab_3);
-        label_9->setObjectName(QStringLiteral("label_9"));
+        label_9->setObjectName(QString::fromUtf8("label_9"));
 
         formLayout_4->setWidget(0, QFormLayout::FieldRole, label_9);
 
@@ -126,7 +126,7 @@ public:
         verticalLayout_4->addLayout(formLayout_4);
 
         horizontalSlider_askedge = new QSlider(tab_3);
-        horizontalSlider_askedge->setObjectName(QStringLiteral("horizontalSlider_askedge"));
+        horizontalSlider_askedge->setObjectName(QString::fromUtf8("horizontalSlider_askedge"));
         horizontalSlider_askedge->setMinimum(5);
         horizontalSlider_askedge->setMaximum(80);
         horizontalSlider_askedge->setValue(20);
@@ -140,18 +140,18 @@ public:
 
         tabWidget_overlays->addTab(tab_3, QString());
         tab_2 = new QWidget();
-        tab_2->setObjectName(QStringLiteral("tab_2"));
+        tab_2->setObjectName(QString::fromUtf8("tab_2"));
         verticalLayout = new QVBoxLayout(tab_2);
-        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
         formLayout_2 = new QFormLayout();
-        formLayout_2->setObjectName(QStringLiteral("formLayout_2"));
+        formLayout_2->setObjectName(QString::fromUtf8("formLayout_2"));
         label_2 = new QLabel(tab_2);
-        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setObjectName(QString::fromUtf8("label_2"));
 
         formLayout_2->setWidget(0, QFormLayout::LabelRole, label_2);
 
         label_6 = new QLabel(tab_2);
-        label_6->setObjectName(QStringLiteral("label_6"));
+        label_6->setObjectName(QString::fromUtf8("label_6"));
 
         formLayout_2->setWidget(0, QFormLayout::FieldRole, label_6);
 
@@ -159,7 +159,7 @@ public:
         verticalLayout->addLayout(formLayout_2);
 
         horizontalSlider_dirthr_up = new QSlider(tab_2);
-        horizontalSlider_dirthr_up->setObjectName(QStringLiteral("horizontalSlider_dirthr_up"));
+        horizontalSlider_dirthr_up->setObjectName(QString::fromUtf8("horizontalSlider_dirthr_up"));
         horizontalSlider_dirthr_up->setMaximum(128);
         horizontalSlider_dirthr_up->setValue(20);
         horizontalSlider_dirthr_up->setOrientation(Qt::Horizontal);
@@ -167,14 +167,14 @@ public:
         verticalLayout->addWidget(horizontalSlider_dirthr_up);
 
         formLayout_3 = new QFormLayout();
-        formLayout_3->setObjectName(QStringLiteral("formLayout_3"));
+        formLayout_3->setObjectName(QString::fromUtf8("formLayout_3"));
         label_3 = new QLabel(tab_2);
-        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setObjectName(QString::fromUtf8("label_3"));
 
         formLayout_3->setWidget(0, QFormLayout::LabelRole, label_3);
 
         label_7 = new QLabel(tab_2);
-        label_7->setObjectName(QStringLiteral("label_7"));
+        label_7->setObjectName(QString::fromUtf8("label_7"));
 
         formLayout_3->setWidget(0, QFormLayout::FieldRole, label_7);
 
@@ -182,7 +182,7 @@ public:
         verticalLayout->addLayout(formLayout_3);
 
         horizontalSlider_dirthr_down = new QSlider(tab_2);
-        horizontalSlider_dirthr_down->setObjectName(QStringLiteral("horizontalSlider_dirthr_down"));
+        horizontalSlider_dirthr_down->setObjectName(QString::fromUtf8("horizontalSlider_dirthr_down"));
         horizontalSlider_dirthr_down->setMaximum(127);
         horizontalSlider_dirthr_down->setOrientation(Qt::Horizontal);
 
@@ -197,14 +197,14 @@ public:
         verticalLayout_3->addWidget(tabWidget_overlays);
 
         horizontalLayout = new QHBoxLayout();
-        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
         pushButton_apply = new QPushButton(Form);
-        pushButton_apply->setObjectName(QStringLiteral("pushButton_apply"));
+        pushButton_apply->setObjectName(QString::fromUtf8("pushButton_apply"));
 
         horizontalLayout->addWidget(pushButton_apply);
 
         pushButton_sticky = new QPushButton(Form);
-        pushButton_sticky->setObjectName(QStringLiteral("pushButton_sticky"));
+        pushButton_sticky->setObjectName(QString::fromUtf8("pushButton_sticky"));
 
         horizontalLayout->addWidget(pushButton_sticky);
 
@@ -213,7 +213,7 @@ public:
         horizontalLayout->addItem(horizontalSpacer);
 
         label_4 = new QLabel(Form);
-        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setObjectName(QString::fromUtf8("label_4"));
 
         horizontalLayout->addWidget(label_4);
 
@@ -236,20 +236,20 @@ public:
 
     void retranslateUi(QWidget *Form)
     {
-        Form->setWindowTitle(QApplication::translate("Form", "Overlays", 0));
+        Form->setWindowTitle(QApplication::translate("Form", "Overlays", 0, QApplication::UnicodeUTF8));
         label_5->setText(QString());
-        label->setText(QApplication::translate("Form", "Window size", 0));
-        tabWidget_overlays->setTabText(tabWidget_overlays->indexOf(tab), QApplication::translate("Form", "Autocorrelate", 0));
-        label_8->setText(QApplication::translate("Form", "Edge Jump Threshold", 0));
+        label->setText(QApplication::translate("Form", "Window size", 0, QApplication::UnicodeUTF8));
+        tabWidget_overlays->setTabText(tabWidget_overlays->indexOf(tab), QApplication::translate("Form", "Autocorrelate", 0, QApplication::UnicodeUTF8));
+        label_8->setText(QApplication::translate("Form", "Edge Jump Threshold", 0, QApplication::UnicodeUTF8));
         label_9->setText(QString());
-        tabWidget_overlays->setTabText(tabWidget_overlays->indexOf(tab_3), QApplication::translate("Form", "AskEdge", 0));
-        label_2->setText(QApplication::translate("Form", "Up", 0));
+        tabWidget_overlays->setTabText(tabWidget_overlays->indexOf(tab_3), QApplication::translate("Form", "AskEdge", 0, QApplication::UnicodeUTF8));
+        label_2->setText(QApplication::translate("Form", "Up", 0, QApplication::UnicodeUTF8));
         label_6->setText(QString());
-        label_3->setText(QApplication::translate("Form", "Down", 0));
+        label_3->setText(QApplication::translate("Form", "Down", 0, QApplication::UnicodeUTF8));
         label_7->setText(QString());
-        tabWidget_overlays->setTabText(tabWidget_overlays->indexOf(tab_2), QApplication::translate("Form", "Dirthreshold", 0));
-        pushButton_apply->setText(QApplication::translate("Form", "Apply", 0));
-        pushButton_sticky->setText(QApplication::translate("Form", "Restore", 0));
+        tabWidget_overlays->setTabText(tabWidget_overlays->indexOf(tab_2), QApplication::translate("Form", "Dirthreshold", 0, QApplication::UnicodeUTF8));
+        pushButton_apply->setText(QApplication::translate("Form", "Apply", 0, QApplication::UnicodeUTF8));
+        pushButton_sticky->setText(QApplication::translate("Form", "Restore", 0, QApplication::UnicodeUTF8));
         label_4->setText(QString());
     } // retranslateUi
 
