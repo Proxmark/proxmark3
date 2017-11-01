83df98d691c002d99d5a8f37d17f9205ba649cc2
Oleg Moiseenko <olegmsn@gmail.com>
2017-11-01 18:44:43 +0200

    change comment in iso14443a.c (#459)

--- armsrc/iso14443a.c
+++ armsrc/iso14443a.c
@@ -1820,7 +1820,7 @@ int iso14443a_select_card(byte_t *uid_ptr, iso14a_card_select_t *p_hi14a_card, u
 		p_hi14a_card->sak = sak;
 	}
 
-	// non iso14443a compliant tag
+	// PICC compilant with iso14443a-4 ---> (SAK & 0x20 != 0)
 	if( (sak & 0x20) == 0) return 2; 
 
 	if (!no_rats) {
