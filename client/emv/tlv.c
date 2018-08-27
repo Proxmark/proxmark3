c1feddf2f3c4b458a698494d563ddf003374a68b
merlokk <olegmsn@gmail.com>
2018-08-27 20:00:35 +0300

    tlvdb_change_or_add_node() works. added tlvdb_find_full()

--- client/emv/tlv.c
+++ client/emv/tlv.c
@@ -318,6 +318,24 @@ struct tlvdb *tlvdb_find(struct tlvdb *tlvdb, tlv_tag_t tag) {
 	return NULL;
 }
 
+struct tlvdb *tlvdb_find_full(struct tlvdb *tlvdb, tlv_tag_t tag) {
+	if (!tlvdb)
+		return NULL;
+	
+	for (; tlvdb; tlvdb = tlvdb->next) {
+		if (tlvdb->tag.tag == tag)
+			return tlvdb;
+		
+		if (tlvdb->children) {
+			struct tlvdb * ch = tlvdb_find_full(tlvdb->children, tag);
+			if (ch)
+				return ch;
+		}			
+	}
+
+	return NULL;
+}
+
 struct tlvdb *tlvdb_find_path(struct tlvdb *tlvdb, tlv_tag_t tag[]) {
 	int i = 0;
 	struct tlvdb *tnext = tlvdb;
@@ -344,7 +362,7 @@ void tlvdb_add(struct tlvdb *tlvdb, struct tlvdb *other)
 
 void tlvdb_change_or_add_node(struct tlvdb *tlvdb, tlv_tag_t tag, size_t len, const unsigned char *value)
 {
-	struct tlvdb *telm = tlvdb_find(tlvdb, tag);
+	struct tlvdb *telm = tlvdb_find_full(tlvdb, tag);
 	if (telm == NULL) {
 		// new tlv element
 		tlvdb_add(tlvdb, tlvdb_fixed(tag, len, value));
@@ -353,19 +371,36 @@ void tlvdb_change_or_add_node(struct tlvdb *tlvdb, tlv_tag_t tag, size_t len, co
 		if (telm->tag.tag == tag && telm->tag.len == len && !memcmp(telm->tag.value, value, len))
 			return;
 
-PrintAndLog("-replace %x", tag);
 		// replace tlv element
 		struct tlvdb *tnewelm = tlvdb_fixed(tag, len, value);
 		tnewelm->next = telm->next;
 		tnewelm->parent = telm->parent;
 		
+		// if elm stays 1st in children link
 		if (telm->parent && telm->parent->children == telm) {
 			telm->parent->children = tnewelm;
 		}
 		
+		// if elm have prev element
+		if (telm != tlvdb) {
+			// elm in root
+			struct tlvdb *celm = tlvdb;
+			// elm in child list of node
+			if (telm->parent && telm->parent->children)
+				celm = telm->parent->children;		
+			
+			// find previous element
+			for (; celm; celm = celm->next) {
+				if (celm->next == telm) {
+					celm->next = tnewelm;
+					break;
+				}
+			}
+		}
 		
-//		telm->next = NULL;
-//		tlvdb_free(telm);
+		// free old element
+		telm->next = NULL;
+		tlvdb_free(telm);
 	}
 	
 	return;
