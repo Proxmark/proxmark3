ec8b84e42fa62dbe87417f137d6d7be5f488f831
merlokk <807634+merlokk@users.noreply.github.com>
2018-10-25 16:26:06 +0300

    added license

--- common/polarssl/aes_cmac128.h
+++ common/polarssl/aes_cmac128.h
@@ -1,3 +1,35 @@
+/*
+ *  AES-CMAC from NIST Special Publication 800-38B — Recommendation for block cipher modes of operation: The CMAC mode for authentication.
+ *
+ *  Copyright (C) 2006-2014, Brainspark B.V.
+ *  Copyright (C) 2014, Anargyros Plemenos
+ *  Tests added Merkok, 2018
+ *
+ *  This file is part of PolarSSL (http://www.polarssl.org)
+ *  Lead Maintainer: Paul Bakker <polarssl_maintainer at polarssl.org>
+ *
+ *  All rights reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  Reference : https://polarssl.org/discussions/generic/authentication-token
+ *  NIST Special Publication 800-38B — Recommendation for block cipher modes of operation: The CMAC mode for authentication.
+ *  Tests here:
+ *  https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/AES_CMAC.pdf
+*/
+
 #include <stdint.h>
 #include <stddef.h>
 #include "aes.h"
