818e15b0c815d25fdfb914cd826baf14b48fa19d
Samuele <sguerrini97@gmail.com>
2019-01-06 18:33:06 +0100

    PCF7931: improved read code and implemented a simple password bruteforce (#745)
    * Improved PCF 7931 read code and implemented a simple PCF7931 password bruteforce
    * Warning on the PCF7931 bruteforce command

--- client/cmdlfpcf7931.h
+++ client/cmdlfpcf7931.h
@@ -1,6 +1,7 @@
 //-----------------------------------------------------------------------------
 // Copyright (C) 2012 Chalk <chalk.secu at gmail.com>
 //				 2015 Dake <thomas.cayrou at gmail.com>
+//				 2018 sguerrini97 <sguerrini97 at gmail.com>
 
 // This code is licensed to you under the terms of the GNU GPL, version 2 or,
 // at your option, any later version. See the LICENSE.txt file for the text of
@@ -26,12 +27,14 @@ int pcf7931_printConfig();
 
 int usage_pcf7931_read();
 int usage_pcf7931_write();
+int usage_pcf7931_bruteforce();
 int usage_pcf7931_config();
 
 int CmdLFPCF7931(const char *Cmd);
 
 int CmdLFPCF7931Read(const char *Cmd);
 int CmdLFPCF7931Write(const char *Cmd);
+int CmdLFPCF7931BruteForce(const char *Cmd);
 int CmdLFPCF7931Config(const char *Cmd);
 
 #endif
