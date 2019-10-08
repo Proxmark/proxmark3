f784539dfbaf5e09a69969e404fa40382687f489
pwpiwi <pwpiwi@users.noreply.github.com>
2019-10-08 11:48:49 +0200

    fix iclass reader functions
    * remove unused CMD_ICLASS_READCHECK
    * fix wrong command coding in CodeIClassCommand()
    * switch field off at end of commands to avoid RDV4 overheating

--- client/hid-flasher/usb_cmd.h
+++ client/hid-flasher/usb_cmd.h
@@ -124,7 +124,6 @@ typedef struct {
 #define CMD_WRITER_LEGIC_RF                                               0x0389
 #define CMD_EPA_PACE_COLLECT_NONCE                                        0x038A
 
-#define CMD_ICLASS_READCHECK                                              0x038F
 #define CMD_ICLASS_CLONE                                                  0x0390
 #define CMD_ICLASS_DUMP                                                   0x0391
 #define CMD_SNOOP_ICLASS                                                  0x0392
